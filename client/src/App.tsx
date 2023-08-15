ba39d1781e1c227ba79558f73fab91f2f7925a4b
Jonathan Alva <john.alvarez.93@gmail.com>
2023-08-14 22:05:08 -0500

    yes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,7 @@ import "./App.css";
 
 function App() {
 	useEffect(() => {
-		fetch("http://127.0.0.1:8080/users", {
-			mode: "cors",
+		fetch("http://127.0.0.1:8080/gymleaders", {
 			headers: {
 				"Content-Type": "application/json",
 			},
